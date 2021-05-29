09447fbc8eb3c1da03c692883db5f5420500ec9b
Maksym Kazarin <kazzarin99@gmail.com>
2021-05-29 12:05:45 +0300

    Main design changes

---
+++ ARfisha/Assets/ButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OpenUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void ShowDetails()
+    {
+        SceneManager.LoadScene("Details");
+        //Scene detailsScene = SceneManager.GetSceneByName("Details");
+        //SceneManager.SetActiveScene(detailsScene);
+    }
+
+    public void ShowAR()
+    {
+        SceneManager.LoadScene("SampleScene");
+        //Scene mainScene = SceneManager.GetSceneByName("SampleScene");
+        //SceneManager.SetActiveScene(mainScene);
+    }
+}
